dea1a3483e805e068bd5b88181a31596d141ef5d
Hassan Essam Mohamed Mohamed <20241700259@cis.asu.edu.eg>
2026-05-03 00:29:51 +0300

    Integrate SVM and Decision Tree models with evaluation
    Added SVM and Decision Tree classifiers with hyperparameter tuning. Enhanced model evaluation metrics including precision, recall, and F1 score.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, precision_score, recall_score
+from sklearn.metrics import f1_score
 
 train_df = pd.read_csv("train_data.csv")
 test_df = pd.read_csv("test_data.csv")
@@ -110,8 +113,197 @@ x_test = x_test.reindex(columns=x_train.columns, fill_value=0)
 print(x_train.isnull().sum())
 print(x_test.isnull().sum())
 
-# scale features to mean=0, std=1
+# scale features to mean=0, std=1   
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
 x_val = scaler.transform(x_val)
 x_test = scaler.transform(x_test)
+
+#Train model
+log_results = []
+c=np.logspace(-4, 2, 30)
+log_solver=['liblinear','lbfgs']
+
+best_c_log=None
+best_log_solver=None
+best_score_log=-1
+
+'''for j in log_solver :
+   for i in c:
+       log_model=LogisticRegression(solver=j,C=i,random_state=0)
+       log_model.fit(x_train, y_train)
+        # Evaluate
+       log_score = log_model.score(x_val, y_val)
+       log_results.append({
+           "C": i,
+           "solver": j,
+           "accuracy": log_score
+       })
+        # Update best Logistic
+       if log_score > best_score_log:
+           best_score_log = log_score
+           best_c_log = i
+           best_log_solver=j
+'''
+
+for j in log_solver:
+    for i in c:
+        log_model = LogisticRegression(solver=j, C=i, random_state=0)
+        log_model.fit(x_train, y_train)
+        y_pred = log_model.predict(x_val)
+        log_score = f1_score(y_val, y_pred)
+        log_results.append({
+            "C": i,
+            "solver": j,
+            "f1": log_score
+        })
+        if log_score > best_score_log:
+            best_score_log = log_score
+            best_c_log = i
+            best_log_solver = j
+
+
+log_model=LogisticRegression(solver=best_log_solver,C=best_c_log,random_state=0)
+log_model.fit(x_train, y_train)
+log_df = pd.DataFrame(log_results)
+
+SVM_results = []
+svm_kernel=['linear','poly','rbf']
+best_c_svm=None
+best_svm_kernel=None
+best_score_svm=-1
+
+for j in svm_kernel :
+   for i in c:
+       svm_model = SVC(C=i, kernel=j, gamma='scale')
+       svm_model.fit(x_train, y_train)
+       y_pred = svm_model.predict(x_val)
+       # Evaluate
+       svm_score = f1_score(y_val, y_pred)
+       SVM_results.append({
+           "C": i,
+           "Kernel": j,
+           "f1": svm_score
+       })
+        # Update best SVM
+       if svm_score > best_score_svm:
+           best_score_svm = svm_score
+           best_c_svm = i
+           best_svm_kernel=j
+
+svm_model=SVC(C=best_c_svm, kernel=best_svm_kernel, gamma='scale')
+svm_model.fit(x_train, y_train)
+SVM_df = pd.DataFrame(SVM_results)
+
+
+DT_results = []
+max_depth_values = [1, 2, 3, 4, 5, 10, 15, 20, None]
+min_samples_split= [2, 5, 10]
+best_min_samples_split=None
+best_max_depth = None
+best_score_DT = -1
+for j in min_samples_split:
+    for i in max_depth_values:
+        DT_model = DecisionTreeClassifier(criterion='gini', max_depth=i, min_samples_split=j,random_state=0)
+        DT_model.fit(x_train,y_train)
+        y_pred = DT_model.predict(x_val)
+        DT_score = f1_score(y_val, y_pred)
+        DT_results.append({
+            "max_depth": i,
+            "min_samples_split": j,
+            "f1": DT_score
+        })
+        if(DT_score > best_score_DT):
+            best_score_DT = DT_score
+            best_max_depth = i
+            best_min_samples_split=j
+
+DT_model = DecisionTreeClassifier(criterion='gini', max_depth=best_max_depth, min_samples_split=best_min_samples_split, random_state=0)
+DT_model.fit(x_train,y_train)
+DT_df = pd.DataFrame(DT_results)
+
+
+# log_params = {
+#     "C": np.logspace(-4, 2, 30),
+#     "solver": ["liblinear", "lbfgs"]
+# }
+#
+# log_grid = GridSearchCV(
+#     LogisticRegression(random_state=0),
+#     param_grid=log_params,
+#     cv=5,
+#     scoring="accuracy"
+# )
+# log_grid.fit(x_train, y_train)
+# log_model=log_grid.best_estimator_
+#
+#
+# svm_params = {
+#     "C": np.logspace(-4, 2, 20),
+#     "kernel": ["linear", 'poly',"rbf"]
+# }
+#
+# svm_grid = GridSearchCV(
+#     SVC(gamma="scale"),
+#     param_grid=svm_params,
+#     cv=5,
+#     scoring="accuracy"
+# )
+# svm_grid.fit(x_train, y_train)
+# svm_model=svm_grid.best_estimator_
+#
+# dt_params = {
+#     "max_depth": [1, 2, 3, 5, 10, 15, None],
+#     "min_samples_split": [2, 5, 10]
+# }
+#
+# DT_grid = GridSearchCV(
+#     DecisionTreeClassifier(random_state=0),
+#     param_grid=dt_params,
+#     cv=5,
+#     scoring="accuracy"
+# )
+#
+# DT_grid.fit(x_train, y_train)
+# DT_model=DT_grid.best_estimator_
+
+# Report & Accuracy
+
+#logistic Regression
+print('Logistic Regression')
+print('Accuracy: '+ str(log_model.score(x_test, y_test)*100)+'%')
+yp_log=log_model.predict(x_test)
+print("F1_Score:"+str(f1_score(y_test, yp_log)*100)+"%")
+print("Precision:"+str(precision_score(y_test, yp_log)*100)+"%")
+print("Recall:"+str(recall_score(y_test, yp_log)*100)+"%")
+print('Confusion Matrix :')
+print(confusion_matrix(y_test, yp_log))
+print('-------------------------------------------------------------')
+
+
+#SVM
+print('SVM')
+print('Accuracy: '+ str(svm_model.score(x_test, y_test)*100)+'%')
+yp_SVM=svm_model.predict(x_test)
+print("F1_Score:"+str(f1_score(y_test, yp_SVM)*100)+"%")
+print("Precision:"+str(precision_score(y_test, yp_SVM)*100)+"%")
+print("Recall:"+str(recall_score(y_test, yp_SVM)*100)+"%")
+print('Confusion Matrix :')
+print(confusion_matrix(y_test, yp_SVM))
+print('-------------------------------------------------------------')
+
+#Decision Tree
+print('Decision Tree ')
+print('Accuracy: '+ str(DT_model.score(x_test, y_test)*100)+'%')
+yp_DT=DT_model.predict(x_test)
+print("F1_Score:"+str(f1_score(y_test, yp_DT)*100)+"%")
+print("Precision:"+str(precision_score(y_test, yp_DT)*100)+"%")
+print("Recall:"+str(recall_score(y_test, yp_DT)*100)+"%")
+print('Confusion Matrix :')
+print(confusion_matrix(y_test, yp_DT))
+print('-------------------------------------------------------------')
+# print(log_df)
+# print('-------------------------------------------------------------')
+# print(SVM_df)
+# print('-------------------------------------------------------------')
+# print(DT_df)
